89eaee339555dfcaf77b20ba44ae0f482e4624cb
blaise <p.elombo@me.com>
2018-06-17 21:05:14 +0200

    save all files

--- src/test/java/testSelenium/TestConnexion.java
+++ src/test/java/testSelenium/TestConnexion.java
@@ -32,7 +32,7 @@ public class TestConnexion {
 
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 
     }
 
